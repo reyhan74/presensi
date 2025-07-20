93a635a90fb9d45627601d60eabf66dcca65eae2
rhn.id <drehan566@gmail.com>
2025-07-20 23:48:16 +0700

    halaman scan

--- app/Views/templates/starting_page_layout.php
+++ app/Views/templates/starting_page_layout.php
@@ -6,50 +6,13 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <?= $this->include('templates/css'); ?>
-   <title>Absensi QR Code</title>
+   <title><?= $title ?? 'Absensi QR Code'; ?></title>
    <style>
-
-
-
-      /* .bg {
-         background: url(<?= base_url('assets/img/bg2.jpg'); ?>) center;
-         opacity: 0.1;
-         background-size: cover;
-         height: 100vh;
-         width: 100%;
-         position: absolute;
-         left: 0;
-         top: 0;
-         z-index: -1;
-      } */
-
       .main-panel {
          position: relative;
          width: 100%;
-         padding-top: 80px; /* agar konten tidak tertutup navbar */
-         transition: 0.33s, cubic-bezier(0.685, 0.0473, 0.346, 1);
-      }
-
-      video#previewKamera {
-         width: 100%;
-         height: auto;
-         margin: 0;
-      }
-
-      .previewParent {
-         width: auto;
-         height: auto;
-         margin: auto;
-         border: 2px solid grey;
-      }
-
-      .unpreview {
-         background-color: aquamarine;
-         text-align: center;
-      }
-
-      .form-select {
-         min-width: 200px;
+         padding-top: 80px;
+         transition: 0.33s cubic-bezier(0.685, 0.0473, 0.346, 1);
       }
 
       .navbar {
@@ -73,28 +36,21 @@
 </head>
 
 <body>
-   <div class="bg bg-image"></div>
-
    <!-- Navbar -->
    <nav class="navbar navbar-expand-lg navbar-light fixed-top">
-   <div class="container-fluid">
-      <div class="navbar-wrapper w-100">
-         <div class="row w-100 align-items-center">
-            <!-- Judul -->
-            <div class="col-12 col-md-6 order-1 order-md-1 text-start">
-               <p class="navbar-brand mb-0"><?= $title ?? 'Login'; ?></p>
-            </div>
-
-            <!-- Tombol di sisi kanan (pindah ke bawah di mobile) -->
-            <div class="col-12 col-md-6 order-2 order-md-2 d-flex justify-content-start justify-content-md-end mt-2 mt-md-0">
-               <?= $this->renderSection('navaction') ?>
+      <div class="container-fluid">
+         <div class="navbar-wrapper w-100">
+            <div class="row w-100 align-items-center">
+               <div class="col-12 col-md-6 order-1 text-start">
+                  <p class="navbar-brand mb-0"><?= $title ?? 'Login'; ?></p>
+               </div>
+               <div class="col-12 col-md-6 order-2 d-flex justify-content-start justify-content-md-end mt-2 mt-md-0">
+                  <?= $this->renderSection('navaction') ?>
+               </div>
             </div>
          </div>
       </div>
-   </div>
-</nav>
-
-   <!-- End Navbar -->
+   </nav>
 
    <div class="main-panel">
       <?= $this->renderSection('content') ?>
@@ -102,5 +58,4 @@
 
    <?= $this->include('templates/js'); ?>
 </body>
-
 </html>
